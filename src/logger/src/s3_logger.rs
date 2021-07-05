b0d061a69a303e7058622b52992e7f25cf997ac2
cstria0106 <cstria0106@gmail.com>
2021-07-06 02:52:58 +0900

    refactor, add log color

--- src/logger/src/s3_logger.rs
+++ src/logger/src/s3_logger.rs
@@ -1,50 +1,71 @@
-use std::io::prelude::*;
+use std::io::{prelude::*, Error};
 
-use chrono::{Datelike, Timelike, Utc};
+use chrono::{DateTime, Utc};
+use colored::*;
 use flate2::write::GzEncoder;
 use flate2::Compression;
-use s3::Bucket;
+use s3::{Bucket, S3Error};
 
 use crate::{log::Log, logger::Logger};
 
 pub struct S3Logger {
     memory_logs: Vec<Log>,
-    bucket: Bucket,
     stdout: bool,
+    bucket: Bucket,
 }
+
+#[derive(Debug)]
+pub enum UploadError {
+    CompressionError(Error),
+    S3Error(S3Error),
+}
+
+pub type UploadResult = Result<Option<String>, UploadError>;
 
 impl S3Logger {
     pub fn new(bucket: Bucket, stdout: Option<bool>) -> S3Logger {
         S3Logger {
             memory_logs: Vec::with_capacity(40960),
+            stdout: stdout.unwrap_or(false),
             bucket,
-            stdout: match stdout {
-                Some(e) => e,
-                _ => false,
-            },
         }
     }
 
-    pub fn upload(&self) {
+    pub fn upload(&self) -> UploadResult {
         let last_log = self.memory_logs.last();
 
-        match last_log {
-            Some(last_log) => {
-                let mut encoder = GzEncoder::new(vec![], Compression::best());
+        if let Some(last_log) = last_log {
+            // Create gzip encoder.
+            let mut encoder = GzEncoder::new(
+                Vec::with_capacity(self.memory_logs.len() * 256),
+                Compression::best(),
+            );
 
-                for log in self.memory_logs.iter() {
-                    encoder
-                        .write_fmt(format_args!("{}\n", log.message()))
-                        .unwrap();
-                }
-
-                let filename = last_log.timestamp().format("%Y%m%d.log.gz").to_string();
-                self.bucket
-                    .put_object_blocking(filename, &encoder.finish().unwrap())
-                    .unwrap();
+            // Compress and write logs.
+            for log in self.memory_logs.iter() {
+                encoder
+                    .write_fmt(format_args!("{}\n", log.message()))
+                    .map_err(|e| UploadError::CompressionError(e))?;
             }
-            _ => {}
+
+            // Encode.
+            let encoded = encoder
+                .finish()
+                .map_err(|e| UploadError::CompressionError(e))?;
+
+            // Format filename.
+            let filename = last_log.timestamp().format("%Y%m%d.log.gz").to_string();
+
+            // Upload.
+            let (_, code) = self
+                .bucket
+                .put_object_blocking(filename.clone(), &encoded)
+                .map_err(|e| UploadError::S3Error(e))?; // TODO: It does not throw error actually
+
+            return Ok(Some(filename));
         }
+
+        Ok(None)
     }
 
     pub fn download() {}
@@ -52,35 +73,50 @@ impl S3Logger {
 
 impl Logger for S3Logger {
     fn log(&mut self, log: Log) {
+        // check that number of days in duration between a and b is more than one day.
+        // fn is_after_a_day(a: &DateTime<Utc>, b: &DateTime<Utc>) -> bool {
+        //     (*a - *b).num_days() > 0
+        // }
+
+        // temporary check function for development.
+        fn is_after_a_day(a: &DateTime<Utc>, b: &DateTime<Utc>) -> bool {
+            true
+        }
+
         let last_log = self.memory_logs.last();
 
-        match last_log {
-            Some(last_log) => {
-                let time = last_log.timestamp();
-                let now = Utc::now();
+        // If this is not first log, check timestamp between last log and current log.
+        let upload_result = if let Some(last_log) = last_log {
+            let time = last_log.timestamp();
+            let now = log.timestamp();
 
-                if time.year() <= now.year()
-                    && time.month() <= now.month()
-                    && time.day() <= now.day()
-                    && time.hour() <= now.hour()
-                    && time.minute() < now.minute()
-                {
-                    if self.stdout {
-                        println!("New log started");
-                        println!("Last log time: {}", time);
-                    }
-
-                    self.upload();
-                    self.memory_logs.clear();
-                }
+            // If last log is old, then upload to S3 and clear logs stored in memory.
+            if is_after_a_day(time, now) {
+                let result = self.upload();
+                self.memory_logs.clear();
+                result
+            } else {
+                Ok(None)
             }
-            _ => {}
-        }
+        } else {
+            Ok(None)
+        };
 
+        // If use stdout, print log in stdout.
         if self.stdout {
-            println!("{} ({})", log.message(), log.timestamp());
+            println!(
+                "{} {}",
+                log.message(),
+                log.timestamp().format("%F %T").to_string().bright_black()
+            );
         }
 
+        println!("{:?}", upload_result);
+
+        // Push logs into memory.
         self.memory_logs.push(log);
+
+        // If upload failed, then panic.
+        upload_result.unwrap();
     }
 }
